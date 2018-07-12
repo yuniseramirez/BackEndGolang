f90dae0c22ea158f80b88873ae432005e40dd526
vanessa Michelle Valoy Vargas <vanessavaloy04@gmail.com>
2018-07-12 11:31:01 -0400

    this is a dummy file

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"github.com/gorilla/handlers"
+
+)
+
+type  Article struct {
+	Title     string   `json:"title, omitempty"`
+	Image     string   `json: "image, omitempty"`
+  Author    string   `json:"author, omitempty"`
+	Paragrath string   `json:"paragrath, omitempty"`
+}
+
+
+
+var articles []Article
+
+
+
+func GetArti(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(articles)
+}
+
+
+func main() {
+	router := mux.NewRouter()
+	articles = append(articles, Article{ Title: "This is the title #1", Image: "I will figure #1", Author:"Vanessa Valoy #1", Paragrath:"This is a paragrath #1"})
+	articles = append(articles, Article{ Title: "This is the title #2", Image: "I will figure #2", Author:"Vanessa Valoy #2", Paragrath:"This is a paragrath #2"})
+  articles = append(articles, Article{ Title: "This is the title #3", Image: "I will figure #3", Author:"Vanessa Valoy #3", Paragrath:"This is a paragrath #3"})
+
+
+	router.HandleFunc("/articles", GetArti).Methods("GET")
+  log.Print("localhost:8000")
+	corsObj := handlers.AllowedOrigins([]string{"*"})
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(corsObj)(router)))
+
+}
